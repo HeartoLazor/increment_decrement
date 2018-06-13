70d3effd1cc344ed53854c28c03bfaa2024378e6
hearto lazor <hearto@kemonogames.com>
2018-06-13 02:08:54 -0400

    Added instructions to script

--- increment_decrement.py
+++ increment_decrement.py
@@ -1,3 +1,14 @@
+#Author: Hearto Lazor
+#Sublime Text plugin to increment/decrement all selected numbers in text.
+#Plugin page: https://github.com/HeartoLazor/increment_decrement
+#Based in https://gist.github.com/dtao/2788978 from https://gist.github.com/dtao
+#Installation:
+#   Place the file under the Packages directory
+#   For example: C:\Users<Username>\AppData\Roaming\Sublime Text 3\Packages\User.
+#   In ST3: Preferences > Key Bindings Add this snippet with your chosen binding:
+#      { "keys": ["ctrl+alt+a"], "command": "increment"},
+#      { "keys": ["ctrl+alt+z"], "command": "decrement"}
+
 import sublime
 import sublime_plugin
 import re
